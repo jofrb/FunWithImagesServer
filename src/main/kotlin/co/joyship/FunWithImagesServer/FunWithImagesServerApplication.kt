2d2558a5331d370b7342b79da1f49aecbada9295
Jfrob <johan@joyship.co>
2019-01-20 15:03:58 +0100

    Initial commit

---
+++ src/main/kotlin/co/joyship/FunWithImagesServer/FunWithImagesServerApplication.kt
@@ -0,0 +1,12 @@
+package co.joyship.FunWithImagesServer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FunWithImagesServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<FunWithImagesServerApplication>(*args)
+}
+
